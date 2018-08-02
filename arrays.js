d8af03c8273776b430b1791fb0c69ddb4b56680d
Cris Hanks <crishanks12@gmail.com>
2018-08-02 20:26:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,36 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice()
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop();
+}
+
+function removeElementFromEndOfArray(array){
+  return array.length -1
+}
+
+
+
+
